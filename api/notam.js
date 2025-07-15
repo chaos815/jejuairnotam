284da7aa63fcfb20824fa88455b23969a300ceb1
chaos815 <wongyun.jung@gmail.com>
2025-07-15 20:23:15 +0900

    Add files via upload

---
+++ api/notam.js
@@ -0,0 +1,27 @@
+
+const https = require('https');
+
+module.exports = async (req, res) => {
+  const { icao } = req.query;
+  if (!icao) {
+    res.status(400).send("Missing ICAO code");
+    return;
+  }
+
+  const url = `https://ourairports.com/airports/${icao}/notams.html`;
+
+  https.get(url, (response) => {
+    let html = "";
+    response.on("data", (chunk) => html += chunk);
+    response.on("end", () => {
+      const match = html.match(/<pre[^>]*>([\s\S]*?)<\/pre>/);
+      if (match && match[1]) {
+        res.status(200).send(match[1]);
+      } else {
+        res.status(404).send("NOTAM data not found");
+      }
+    });
+  }).on("error", (err) => {
+    res.status(500).send("Error fetching NOTAM data");
+  });
+};
